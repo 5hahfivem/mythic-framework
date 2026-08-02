c021c8ba372ed729558374420fca22d2d87ecc31
5hahfivem <181221230+5hahfivem@users.noreply.github.com>
2026-08-02 15:54:35 +1000

    chore(base): rewrite sub-optimal callback systems

--- resources/[mythic]/mythic-base/core/cl_callback.lua
+++ resources/[mythic]/mythic-base/core/cl_callback.lua
@@ -1,51 +1,89 @@
 local _sCallbacks = {}
 local _cCallbacks = {}
+local _callbackId = 0
+local _callbackTimeout = GetConvarInt('callback_timeout', 300000)
+
+local function stackTrace()
+    return Citizen.InvokeNative(`FORMAT_STACK_TRACE` & 0xFFFFFFFF, nil, 0, Citizen.ResultAsString()) or ''
+end
+
+local function generateId(event, extraId)
+    _callbackId += 1
+
+    if extraId ~= nil then
+        return string.format("%s-%s-%s", event, extraId, _callbackId)
+    end
+
+    return string.format("%s-%s", event, _callbackId)
+end
 
 COMPONENTS.Callbacks = {
     _required = { 'ServerCallback', 'RegisterClientCallback', 'DoClientCallback' },
     _name = 'base',
     ServerCallback = function(self, event, data, cb, extraId)
-        id = event
-        if extraId ~= nil then
-            id = string.format("%s-%s", event, extraId)
-        else
-            extraId = ''
-        end
+        local id = generateId(event, extraId)
 
         data = data or {}
         _sCallbacks[id] = cb
-        TriggerServerEvent('Callbacks:Server:TriggerEvent', event, data, extraId)
+        TriggerServerEvent('Callbacks:Server:TriggerEvent', event, data, id)
+
+        if cb == nil then return end
+
+        SetTimeout(_callbackTimeout, function()
+            if _sCallbacks[id] ~= nil then
+                _sCallbacks[id] = nil
+                COMPONENTS.Logger:Warn('Callbacks', string.format("Server Callback Timed Out: ^2%s^7", event), { console = true })
+            end
+        end)
+    end,
+    ServerCallbackSync = function(self, event, data, extraId)
+        local p = promise.new()
+
+        self:ServerCallback(event, data, function(...)
+            p:resolve({ ... })
+        end, extraId)
+
+        SetTimeout(_callbackTimeout, function()
+            p:reject(string.format("Server Callback Timed Out: %s", event))
+        end)
+
+        return table.unpack(Citizen.Await(p))
     end,
     RegisterClientCallback = function(self, event, cb)
         _cCallbacks[event] = cb
     end,
-    DoClientCallback = function(self, event, extraId, data)
-        if _cCallbacks[event] ~= nil then
-            CreateThread(function()
-                _cCallbacks[event](data, function(...)
-                    TriggerServerEvent('Callbacks:Server:ReceiveCallback', event, extraId, ...)
-                end)
-            end)
+    DoClientCallback = function(self, event, id, data)
+        local callback = _cCallbacks[event]
+        if callback == nil then
+            COMPONENTS.Logger:Warn('Callbacks', string.format("Attempt To Trigger Non-Existent Client Callback: ^2%s^7", event), { console = true })
+            TriggerServerEvent('Callbacks:Server:ReceiveCallback', event, id)
+            return
         end
+
+        CreateThread(function()
+            local success, err = pcall(callback, data, function(...)
+                TriggerServerEvent('Callbacks:Server:ReceiveCallback', event, id, ...)
+            end)
+
+            if not success then
+                COMPONENTS.Logger:Error('Callbacks', string.format("Client Callback ^2%s^7 Failed: %s\n%s", event, err, stackTrace()), { console = true })
+                TriggerServerEvent('Callbacks:Server:ReceiveCallback', event, id)
+            end
+        end)
     end,
 }
 
 RegisterNetEvent('Callbacks:Client:TriggerEvent')
-AddEventHandler('Callbacks:Client:TriggerEvent', function(event, data, extraId)
+AddEventHandler('Callbacks:Client:TriggerEvent', function(event, data, id)
     if data == nil then data = {} end
-    COMPONENTS.Callbacks:DoClientCallback(event, extraId, data)
+    COMPONENTS.Callbacks:DoClientCallback(event, id, data)
 end)
 
 RegisterNetEvent('Callbacks:Client:ReceiveCallback')
-AddEventHandler('Callbacks:Client:ReceiveCallback', function(event, extraId, ...)
-    local id = event
-    if extraId ~= '' then
-        id = string.format("%s-%s", event, extraId)
-    end
+AddEventHandler('Callbacks:Client:ReceiveCallback', function(event, id, ...)
+    local callback = _sCallbacks[id]
+    if callback == nil then return end
 
-    if _sCallbacks[id] ~= nil then
-        _sCallbacks[id](...)
-        _sCallbacks[id] = nil
-        collectgarbage()
-    end
+    _sCallbacks[id] = nil
+    callback(...)
 end)
--- resources/[mythic]/mythic-base/core/sv_callback.lua
+++ resources/[mythic]/mythic-base/core/sv_callback.lua
@@ -1,5 +1,21 @@
 local _sCallbacks = {}
 local _cCallbacks = {}
+local _callbackId = 0
+local _callbackTimeout = GetConvarInt('callback_timeout', 300000)
+
+local function stackTrace()
+    return Citizen.InvokeNative(`FORMAT_STACK_TRACE` & 0xFFFFFFFF, nil, 0, Citizen.ResultAsString()) or ''
+end
+
+local function generateId(event, extraId)
+    _callbackId += 1
+
+    if extraId ~= nil then
+        return string.format("%s-%s-%s", event, extraId, _callbackId)
+    end
+
+    return string.format("%s-%s", event, _callbackId)
+end
 
 COMPONENTS.Callbacks = {
     _required = { 'RegisterServerCallback', 'DoServerCallback', 'ClientCallback' },
@@ -7,47 +23,78 @@ COMPONENTS.Callbacks = {
     RegisterServerCallback = function(self, event, cb)
         _sCallbacks[event] = cb
     end,
-    DoServerCallback = function(self, source, event, data, extraId)
-        if _sCallbacks[event] ~= nil then
-            CreateThread(function()
-                _sCallbacks[event](source, data, function(...)
-                    TriggerClientEvent('Callbacks:Client:ReceiveCallback', source, event, extraId, ...)
-                end)
-            end)
+    DoServerCallback = function(self, source, event, data, id)
+        local callback = _sCallbacks[event]
+        if callback == nil then
+            COMPONENTS.Logger:Warn('Callbacks', string.format("Attempt To Trigger Non-Existent Server Callback: ^2%s^7", event), { console = true })
+            TriggerClientEvent('Callbacks:Client:ReceiveCallback', source, event, id)
+            return
         end
+
+        CreateThread(function()
+            local success, err = pcall(callback, source, data, function(...)
+                TriggerClientEvent('Callbacks:Client:ReceiveCallback', source, event, id, ...)
+            end)
+
+            if not success then
+                COMPONENTS.Logger:Error('Callbacks', string.format("Server Callback ^2%s^7 Failed: %s\n%s", event, err, stackTrace()), { console = true })
+                TriggerClientEvent('Callbacks:Client:ReceiveCallback', source, event, id)
+            end
+        end)
     end,
     ClientCallback = function(self, source, event, data, cb, extraId)
         if data == nil then data = {} end
 
-        id = string.format("%s", event)
-        if extraId ~= nil then
-            id = string.format("%s-%s", event, extraId)
-        else
-            extraId = ''
-        end
+        local id = generateId(event, extraId)
 
         _cCallbacks[source] = _cCallbacks[source] or {}
         _cCallbacks[source][id] = cb
-        TriggerClientEvent('Callbacks:Client:TriggerEvent', source, event, data, extraId)
+        TriggerClientEvent('Callbacks:Client:TriggerEvent', source, event, data, id)
+
+        if cb == nil then return end
+
+        SetTimeout(_callbackTimeout, function()
+            if _cCallbacks[source] ~= nil and _cCallbacks[source][id] ~= nil then
+                _cCallbacks[source][id] = nil
+                COMPONENTS.Logger:Warn('Callbacks', string.format("Client Callback Timed Out: ^2%s^7", event), { console = true })
+            end
+        end)
+    end,
+    ClientCallbackSync = function(self, source, event, data, extraId)
+        local p = promise.new()
+
+        self:ClientCallback(source, event, data, function(...)
+            p:resolve({ ... })
+        end, extraId)
+
+        SetTimeout(_callbackTimeout, function()
+            p:reject(string.format("Client Callback Timed Out: %s", event))
+        end)
+
+        return table.unpack(Citizen.Await(p))
     end
 }
 
-RegisterServerEvent('Callbacks:Server:TriggerEvent', function(event, data, extraId)
+RegisterServerEvent('Callbacks:Server:TriggerEvent', function(event, data, id)
     data = data or {}
-    COMPONENTS.Callbacks:DoServerCallback(source, event, data, extraId)
+    COMPONENTS.Callbacks:DoServerCallback(source, event, data, id)
 end)
 
-RegisterServerEvent('Callbacks:Server:ReceiveCallback', function(event, extraId, ...)
+RegisterServerEvent('Callbacks:Server:ReceiveCallback', function(event, id, ...)
     local src = source
-    local id = event
 
-	if extraId ~= nil and extraId ~= "" then
-        id = string.format("%s-%s", event, extraId)
-	end
+    local pending = _cCallbacks[src]
+    if pending == nil then return end
 
-    _cCallbacks[src] = _cCallbacks[src] or {}
-    if _cCallbacks[src][id] ~= nil then
-        _cCallbacks[src][id](...)
-        _cCallbacks[src][id] = nil
-    end
+    local callback = pending[id]
+    if callback == nil then return end
+
+    pending[id] = nil
+    callback(...)
+end)
+
+AddEventHandler('Proxy:Shared:RegisterReady', function()
+    COMPONENTS.Middleware:Add('playerDropped', function(source)
+        _cCallbacks[source] = nil
+    end)
 end)
