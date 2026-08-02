da96c1ce816337547491d1ee1b1e1302221046db
eh <coreykemn@gmail.com>
2026-08-02 19:39:08 +1000

    Update sv_logger.lua

--- resources/[mythic]/mythic-base/components/sv_logger.lua
+++ resources/[mythic]/mythic-base/components/sv_logger.lua
@@ -53,12 +53,8 @@ local function doLog(level, component, log, flags, data)
 			local currDate = os.date("%Y-%m-%d")
 			local timestamp = os.date("%I:%M:%S %p")
 			local logDir = ("logs/%s"):format(component)
-			if package.config:sub(1, 1) == "\\" then
-				os.execute(("mkdir \"%s\""):format("logs"))
-				os.execute(("mkdir \"%s\""):format(logDir:gsub("/", "\\\\")))
-			else
-				os.execute(("mkdir -p \"%s\""):format(logDir))
-			end
+			os.execute(("mkdir \"%s\""):format("logs"))
+			os.execute(("mkdir \"%s\""):format(logDir))
 			local logFile, errorReason = io.open(("%s/%s.log"):format(logDir, currDate), "a")
 			if not logFile then
 				return print(errorReason)
